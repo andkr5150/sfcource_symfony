5010c2dba9ac79ab2fe3ee90d0447b0c8747529b
andrii <andkr5150@gmail.com>
2021-03-05 20:39:43 -0800

    update_1 controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,19 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
+        return $this->render("home/index.html.twig");
+    }
+
+    /**
+     * @Route("/custom/{name}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request)
+    {
+        $name = $request->get("name");
+        return $this->render("home/custom.html.twig", [
+            "name" => $name,
         ]);
     }
 }
